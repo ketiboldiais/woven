15229cfcc48f21dbc9396a92d3107ffa477ee1f4
Ketib Oldiais <numerstack@gmail.com>
2024-02-22 14:41:35 -0600

    remove algebra modules

--- src/components/FPlot2D.tsx
+++ src/components/FPlot2D.tsx
@@ -6,12 +6,16 @@ export const FunctionPlotter = () => {
   const [f, setF] = useState('');
   const [plot, setPlot] = useState(false);
   const render = () => setPlot(true);
+  const updateF = (value:string) => {
+    setPlot(false);
+    setF(value);
+  }
   return (
     <div>
       <TextInput 
 				label={'Function'} 
 				value={f} 
-				onChange={(e) => setF(e.target.value)}
+				onChange={(e) => updateF(e.target.value)}
 			/>
       <button onClick={() => render()}>Render</button>
       {plot && f && <FPlot2D fn={f} />}
